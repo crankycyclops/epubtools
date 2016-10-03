ffb48361c980f32eb3e63a51524b12aa3af38d64
James Colannino <crankycyclops@gmail.com>
2016-10-03 09:49:58 -0700

    Fixed a bunch of formatting errors (Abiword conversion to Latex sucks...)

--- drivers/abiword.py
+++ drivers/abiword.py
@@ -5,17 +5,18 @@ import util, driver
 
 class Abiword(driver.Driver):
 
+	# Note that order is important here!
 	specialChars = {
+		"''":       "&#8221;", #rdquo
 		"'":        "&#8217;", #rsquo
 		"{`}":      "&#8216;", #lsquo
-		"''":       "&#8221;", #rdquo
 		"{``}":     "&#8220;", #ldquo
 		"\ldots{}": "&#8230;", #hellip
 		"---":      "&#8212;", #mdash
-		"--":       "&#8211;", #ndash
 		"™":        "&#8482;", #trade
 		"©":        "&#169;",  #copy
-		"®":        "&#174;"   #reg
+		"®":        "&#174;",  #reg
+		"--":       "&#8211;"  #ndash
 	}
 
 	##########################################################################
@@ -61,6 +62,8 @@ class Abiword(driver.Driver):
 
 	# Processes a chapter from any document type that Abiword can read (.doc,
 	# .docx, .rtf, etc.) and transforms it into ePub friendly XHTML.
+	# Apparently, Abiword's conversion to Latex is a pile of s**t, so I'm
+	# limited in what I can fix :'(
 	def transformChapter(self, inputText):
 
 		inParagraph = False
@@ -88,6 +91,20 @@ class Abiword(driver.Driver):
 		underlineRegex = re.compile('\\\\uline{(.*?)}')
 		textttRegex = re.compile('\\\\texttt{(.*?)}')
 		spacingRegex = re.compile('\\\\begin{spacing}{.*?}(.*?)\\\\end{spacing}')
+		hypertargetRegex = re.compile('\\\\hypertarget{.*?}{(.*?)}')
+
+		# As far as I can tell, this is Abiword f*cking up... Run these in order.
+		hypertargetBrokenRegex = re.compile('\\\\hypertarget{.*?}{}(.*?)\\\\hypertarget{.*?}{')
+		hypertargetBrokenRegex2 = re.compile('}(.*?)\\\\hypertarget{.*?}{')
+		hypertargetBrokenRegex3 = re.compile('(.*?)\\\\hypertarget{.*?}{')
+
+		# These are a best guess in an attempt to clean up Abiword's mess. There will, of course,
+		# be edge cases where the user intended to have unbalanced braces. Oh well, not my fault.
+		# Abiword is just kind of garbage when it comes to the conversion of Word Docs to Latex.
+		# This at least limits the removal to only unbalanced parenthesis at the beginning or
+		# end of the string, which minimizes false positives.
+		braceFixRegex1 = re.compile('(<p>[^{]+?)}</p>')
+		braceFixRegex2 = re.compile('<p>{([^}]+?</p>)')
 
 		invalidSlugCharsRegex = re.compile('[^a-zA-Z0-9]')
 
@@ -98,7 +115,16 @@ class Abiword(driver.Driver):
 			'%paragraphs': ''
 		}
 
+		#import pprint, sys
+		#pprint.pprint(paragraphs)
+		#sys.exit(0)
+
 		for i in range(0, len(paragraphs)):
+
+			# Replace latex constructs inside each paragraph with XHTML equivalents
+			paragraphs[i] = emphRegex.sub(r'<em>\1</em>', paragraphs[i])
+			paragraphs[i] = boldRegex.sub(r'<strong>\1</strong>', paragraphs[i])
+			paragraphs[i] = underlineRegex.sub(r'<span style="font-decoration: underline;">\1</span>', paragraphs[i])
 
 			# Strip out \texttt if present
 			paragraphs[i] = textttRegex.sub(r'\1', paragraphs[i])
@@ -106,14 +132,21 @@ class Abiword(driver.Driver):
 			# Strip out spacing directives
 			paragraphs[i] = spacingRegex.sub(r'\1', paragraphs[i])
 
+			# Strip out hypertags if present
+			paragraphs[i] = hypertargetBrokenRegex.sub(r'\1', paragraphs[i])
+			paragraphs[i] = hypertargetBrokenRegex2.sub(r'\1', paragraphs[i])
+			paragraphs[i] = hypertargetBrokenRegex3.sub(r'\1', paragraphs[i])
+			paragraphs[i] = hypertargetRegex.sub(r'\1', paragraphs[i])
+
+			print(paragraphs[i])
+
+			# Attempt an imperfect fix for Abiword f**kery
+			paragraphs[i] = braceFixRegex1.sub(r'\1</p>', paragraphs[i])
+			paragraphs[i] = braceFixRegex2.sub(r'<p>\1', paragraphs[i])
+
 			# Next, replace common Latex entities with XHTML entities (this won't catch everything)
 			for char in self.specialChars.keys():
 				paragraphs[i] = paragraphs[i].replace(char, self.specialChars[char])
-
-			# Now, replace latex constructs inside each paragraph with XHTML equivalents
-			paragraphs[i] = emphRegex.sub(r'<em>\1</em>', paragraphs[i])
-			paragraphs[i] = boldRegex.sub(r'<strong>\1</strong>', paragraphs[i])
-			paragraphs[i] = underlineRegex.sub(r'<span style="font-decoration: underline;">\1</span>', paragraphs[i])
 
 			# The first paragraph is the title, so don't add it to the body
 			if i != 0:
